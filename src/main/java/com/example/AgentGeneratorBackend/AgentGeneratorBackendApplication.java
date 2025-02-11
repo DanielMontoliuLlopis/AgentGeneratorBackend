3007d276fe5bc3324dce28512a75626442aff6a2
Daniel Montoliu <danllo@alu.iesserpis.org>
2025-02-11 23:01:36 +0100

    Initial Commit

---
+++ src/main/java/com/example/AgentGeneratorBackend/AgentGeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgentGeneratorBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentGeneratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentGeneratorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AgentGeneratorBackend/AgentGeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgentGeneratorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentGeneratorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
